34172a13a4c98b57f84b1b73b0a6bf0e91cf8837
ENigmatov <enigmatov82@gmail.com>
2022-11-28 09:50:17 +0500

    # Добавляем адрес страницы и заголовки

--- main.py
+++ main.py
@@ -19,5 +19,15 @@ print('''
    ##        ##     ## ##     ##  ######  ######## ##     ##     ######   #######  ##     ##  #######  ##    ## ###    ##     ######  
      ''')
 
+# Добавляем адрес страницы и заголовки
+headers = {'Accept': '*/*',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
+# url = r'https://somon.tj/telefonyi-i-svyaz/hudzhand/'
+# url = r"https://somon.tj/transport/"
+# url = r'https://somon.tj/vakansii/'
+# url = r'https://somon.tj/nedvizhimost/'
+# url = r'https://somon.tj/kompyuteryi-i-orgtehnika/'
+# url = r'https://somon.tj/biznes-i-uslugi/'
+url = input('Введите аддрес сайта (пример: https://somon.tj/telefonyi-i-svyaz/\n\n')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
