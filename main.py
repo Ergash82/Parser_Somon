a7932782923e01ff2aa7eeeb446d70c53794c080
ENigmatov <enigmatov82@gmail.com>
2022-11-28 09:25:22 +0500

    Импортируем необходимые модули

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Импортируем необходимые модули
+from bs4 import BeautifulSoup as bs
+import requests as req
+import time
+from tqdm import tqdm  # progress bar
+import pandas as pd
+import openpyxl
+import json
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
