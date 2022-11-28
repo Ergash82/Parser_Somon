dcd0c8ae41dd869978b025596d555dda89309e13
ENigmatov <enigmatov82@gmail.com>
2022-11-28 11:20:28 +0500

    Получаем сылку на рубрику и подрубрику

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ import json
 
 # Заголовок программы
 def main():
+    # Заголовок программы
     print('''
-       ########     ###    ########   ######  ######## ########      ######   #######  ##     ##  #######  ##    ##     ########       ## 
-       ##     ##   ## ##   ##     ## ##    ## ##       ##     ##    ##    ## ##     ## ###   ### ##     ## ###   ##        ##          ## 
-       ##     ##  ##   ##  ##     ## ##       ##       ##     ##    ##       ##     ## #### #### ##     ## ####  ##        ##          ## 
-       ########  ##     ## ########   ######  ######   ########      ######  ##     ## ## ### ## ##     ## ## ## ##        ##          ## 
-       ##        ######### ##   ##         ## ##       ##   ##            ## ##     ## ##     ## ##     ## ##  ####        ##    ##    ## 
-       ##        ##     ## ##    ##  ##    ## ##       ##    ##     ##    ## ##     ## ##     ## ##     ## ##   ### ###    ##    ##    ## 
-       ##        ##     ## ##     ##  ######  ######## ##     ##     ######   #######  ##     ##  #######  ##    ## ###    ##     ######  
-         ''')
+    ########     ###    ########   ######  ######## ########      ######   #######  ##     ##  #######  ##    ##     ########       ## 
+    ##     ##   ## ##   ##     ## ##    ## ##       ##     ##    ##    ## ##     ## ###   ### ##     ## ###   ##        ##          ## 
+    ##     ##  ##   ##  ##     ## ##       ##       ##     ##    ##       ##     ## #### #### ##     ## ####  ##        ##          ## 
+    ########  ##     ## ########   ######  ######   ########      ######  ##     ## ## ### ## ##     ## ## ## ##        ##          ## 
+    ##        ######### ##   ##         ## ##       ##   ##            ## ##     ## ##     ## ##     ## ##  ####        ##    ##    ## 
+    ##        ##     ## ##    ##  ##    ## ##       ##    ##     ##    ## ##     ## ##     ## ##     ## ##   ### ###    ##    ##    ## 
+    ##        ##     ## ##     ##  ######  ######## ##     ##     ######   #######  ##     ##  #######  ##    ## ###    ##     ######  
+      ''')
 
     # Добавляем адрес страницы и заголовки
     headers = {'Accept': '*/*',
@@ -40,61 +41,60 @@ def main():
 
         # Парсим первую страниу и получаем список рубрик
 
-        # 1 Получаем имя Города
+        # 2 Получаем имя Города
         def get_city():
             ''' Запрос и Получение Города'''
             rubrics_city = []
-            rubrics_cities_list = soup('ul', class_="rubric-cities-list")
+            rubrics_cities_list = soup.find_all('ul', class_="rubric-cities-list")
             for rubric_city in rubrics_cities_list:
                 for j, link in enumerate(rubric_city.findAll('li', class_="rubric-cities-list-item")):
                     refr_city = link.find('a')['href']
                     rubrics_city.append(refr_city.split('/')[2])
                     print(f'№:{j} - {refr_city.split("/")[2]}')
-            sel_city = int(input('Выберите Город! №:\n'))
-            print(rubrics_city[sel_city])
-            return rubrics_city[sel_city]
+            select_city = int(input('Выберите Город! №:\n'))
+            print(rubrics_city[select_city])
+            return rubrics_city[select_city]
 
-        # 2 Получаем Рубрику
+        # 1 Получаем Рубрику
         def get_rubrics():
-            global soup
-            global sel_rubric
+            # global soup
             rubrics.clear()
-            sel_city = get_city()
+            select_city = get_city()
             for rubric in soup.findAll(class_="rubrics-list"):
                 for i, link in enumerate(rubric.findAll('li')):
                     refer = link.find('a')['href']
-                    rubrics.append([refer.split('/')[2], "https://somon.tj" + refer + sel_city])
+                    rubrics.append([refer.split('/')[2], "https://somon.tj" + refer + select_city])
                     print(f'№:{i} - {refer.split("/")[2]}')
 
             # Выбираем рубрику
-            sel_rubric = int(input('Выберите рубрику №: ! \n'))
-            print(rubrics[sel_rubric][0])
-            return rubrics[sel_rubric][1]
+            select_rubric = int(input('Выберите рубрику №: ! \n'))
+            print(rubrics[select_rubric][0])
+            return rubrics[select_rubric][1]
 
         # 3 Получаем под-Рубрику
-        def get_sub_rubric():
-            global soup
-            global sel_rubric
+        def get_sub_rubric(soup):
+            # global soup
+            # global sel_rubric
             rubrics.clear()
             for rubric in soup.findAll(class_="rubrics-list"):
                 for link in rubric.findAll('li'):
-                    refr = link.find('a')['href']
-                    rubrics.append([refr.split('/')[3], "https://somon.tj" + refr])
+                    refer = link.find('a')['href']
+                    rubrics.append([refer.split('/')[3], "https://somon.tj" + refer])
 
             for i, rub in enumerate(rubrics):
                 print(f'№:{i} - {rub[0]}')
                 # Выбираем рубрику
-            selet_rubrics = int(input('Выберите рубрику №: ! \n'))
-            sel_rubric = selet_rubrics
-            print(rubrics[sel_rubric][0], rubrics[sel_rubric][1])
-            html = req.get(rubrics[sel_rubric][1], headers)
+            select_rubric = int(input('Выберите рубрику №: ! \n'))
+
+            print(rubrics[select_rubric][0], rubrics[select_rubric][1])
+            html = req.get(rubrics[select_rubric][1], headers)
             soup = bs(html.text, 'lxml')
 
         rubric_link = get_rubrics()
         html = req.get(rubric_link, headers)
         if bs(html.text, 'lxml').find(class_="rubrics-list").findAll('li'):
             soup = bs(html.text, 'lxml')
-            get_sub_rubric()  # Парсим под рубрику если она имеется!
+            get_sub_rubric(soup)  # Парсим под рубрику если она имеется!
         else:
             print('Под_рубрики не найдены!')
 
