5dc9cb269ecefafb7b387206db499de28b977ef3
ENigmatov <enigmatov82@gmail.com>
2022-11-28 09:56:26 +0500

    Заключаем код в функйию main

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ import openpyxl
 import json
 
 # Заголовок программы
-print('''
-   ########     ###    ########   ######  ######## ########      ######   #######  ##     ##  #######  ##    ##     ########       ## 
-   ##     ##   ## ##   ##     ## ##    ## ##       ##     ##    ##    ## ##     ## ###   ### ##     ## ###   ##        ##          ## 
-   ##     ##  ##   ##  ##     ## ##       ##       ##     ##    ##       ##     ## #### #### ##     ## ####  ##        ##          ## 
-   ########  ##     ## ########   ######  ######   ########      ######  ##     ## ## ### ## ##     ## ## ## ##        ##          ## 
-   ##        ######### ##   ##         ## ##       ##   ##            ## ##     ## ##     ## ##     ## ##  ####        ##    ##    ## 
-   ##        ##     ## ##    ##  ##    ## ##       ##    ##     ##    ## ##     ## ##     ## ##     ## ##   ### ###    ##    ##    ## 
-   ##        ##     ## ##     ##  ######  ######## ##     ##     ######   #######  ##     ##  #######  ##    ## ###    ##     ######  
-     ''')
+def main():
+    print('''
+       ########     ###    ########   ######  ######## ########      ######   #######  ##     ##  #######  ##    ##     ########       ## 
+       ##     ##   ## ##   ##     ## ##    ## ##       ##     ##    ##    ## ##     ## ###   ### ##     ## ###   ##        ##          ## 
+       ##     ##  ##   ##  ##     ## ##       ##       ##     ##    ##       ##     ## #### #### ##     ## ####  ##        ##          ## 
+       ########  ##     ## ########   ######  ######   ########      ######  ##     ## ## ### ## ##     ## ## ## ##        ##          ## 
+       ##        ######### ##   ##         ## ##       ##   ##            ## ##     ## ##     ## ##     ## ##  ####        ##    ##    ## 
+       ##        ##     ## ##    ##  ##    ## ##       ##    ##     ##    ## ##     ## ##     ## ##     ## ##   ### ###    ##    ##    ## 
+       ##        ##     ## ##     ##  ######  ######## ##     ##     ######   #######  ##     ##  #######  ##    ## ###    ##     ######  
+         ''')
 
-# Добавляем адрес страницы и заголовки
-headers = {'Accept': '*/*',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
-# url = r'https://somon.tj/telefonyi-i-svyaz/hudzhand/'
-# url = r"https://somon.tj/transport/"
-# url = r'https://somon.tj/vakansii/'
-# url = r'https://somon.tj/nedvizhimost/'
-# url = r'https://somon.tj/kompyuteryi-i-orgtehnika/'
-# url = r'https://somon.tj/biznes-i-uslugi/'
-url = input('Введите аддрес сайта (пример: https://somon.tj/telefonyi-i-svyaz/\n\n')
+    # Добавляем адрес страницы и заголовки
+    headers = {'Accept': '*/*',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
+    # url = r'https://somon.tj/telefonyi-i-svyaz/hudzhand/'
+    # url = r"https://somon.tj/transport/"
+    # url = r'https://somon.tj/vakansii/'
+    # url = r'https://somon.tj/nedvizhimost/'
+    # url = r'https://somon.tj/kompyuteryi-i-orgtehnika/'
+    # url = r'https://somon.tj/biznes-i-uslugi/'
+    url = input('Введите аддрес сайта (пример: https://somon.tj/telefonyi-i-svyaz/\n\n')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
